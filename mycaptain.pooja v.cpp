8d4a8032934600b925a110ff8362b199b703f93a
poojayadhav567 <92743586+poojayadhav567@users.noreply.github.com>
2021-10-19 22:24:30 +0530

    Add files via upload

---
+++ mycaptain.pooja v.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"size of int :"<<sizeof(int)<<"\n";
+	cout<<"size of char :"<<sizeof(char)<<"\n";
+	cout<<"size of float :"<<sizeof(float)<<"\n";
+	cout<<"size of double :"<<sizeof(double)<<"\n";
+	return 0;
+}
